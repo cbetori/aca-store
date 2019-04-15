0944bc4b1a56a14b2929065b710c4948085489de
Collin Betori <cbetori@gmail.com>
2019-04-15 07:53:01 -0500

    minor update

--- public/index.js
+++ public/index.js
@@ -1,48 +1,87 @@
 //Creates list on load
-function productList(products){
+let list = "List";
 
-    let containerNames = document.getElementById('names')
-    let containerButtons = document.getElementById('buttons')
+function productList(products, list) {
+  let containerNames = document.getElementById("names" + list);
+  let containerButtons = document.getElementById("buttons" + list);
 
-    products.map((element, index)=>{ 
+  products.map((element, index) => {
+    let li = document.createElement("li");
+    li.setAttribute("id", list + element._id);
+    li.setAttribute("class", "nameList");
+    containerNames.appendChild(li);
+    li.innerHTML = element.name;
 
-        let li = document.createElement('li')
-        li.setAttribute('value', element._id)
-        li.setAttribute('class', 'nameList')
-        containerNames.appendChild(li)
-        li.innerHTML = element.name
-
-        let button = document.createElement('button')
-        button.setAttribute('value', element._id)
-        button.setAttribute('class', 'buttonList')
-        containerButtons.appendChild(button)
-        button.innerHTML = 'Add to Cart'
-    })
-
-    document.getElementById('handleSearch').onclick = searchBox=>{ 
-        let searchValue = document.getElementById('searchBox').value
-        searchProducts(searchValue)
+    let button = document.createElement("button");
+    button.setAttribute("value", element._id);
+    containerButtons.appendChild(button);
+    button.setAttribute("class", "buttonList");
+    if (list === "List") {
+      button.innerHTML = "Add to Cart";
+    } else {
+      button.innerHTML = "Remove";
     }
-    
-    let buttonClass = document.getElementsByClassName('buttonList')
-    for(let i = 0;i<buttonClass.length; i++){
-        buttonClass[i].onclick = function addToCart(){
-        console.log(buttonClass[i].value)
-        }
-    }
+  });
+
+  document.getElementById("handleSearch").onclick = searchBox => {
+    let searchValue = document.getElementById("searchBox").value;
+    searchProducts(searchValue);
+  };
+
+  let buttonClass = document.getElementsByClassName("buttonList");
+  for (let i = 0; i < buttonClass.length; i++) {
+    buttonClass[i].onclick = function handleCart() {
+      if (buttonClass[i].innerHTML === "Add to Cart") {
+        addToCart(parseInt(buttonClass[i].value));
+      } else {
+        console.log(document.getElementById("shoppingCart"));
+        buttonClass[i].innerHTML = "";
+        document.getElementById("Cart" + (i + 1)).innerHTML = "";
+      }
+    };
+  }
+
+  let shoppingCartBtn = document.getElementById("shoppingCartBtn");
+  let cartView = document.getElementById("shoppingCart");
+  shoppingCartBtn.onclick = () => {
+    toggleCartView(cartView);
+  };
 }
 
-window.onload = ()=> productList(products)
+window.onload = () => productList(products, list);
 
 //Search
-const searchProducts = (searchValue) =>{
-    let filterredProducts = products.filter((p)=>{
-        return p.name === searchValue
-    })
-    productList(filterredProducts)
-}
+const searchProducts = searchValue => {
+  let filteredProducts = products.filter(p => {
+    return p.name === searchValue;
+  });
+  productList(filteredProducts, list);
+};
 
 //Shopping Cart
+const toggleCartView = cartView => {
+  if (cartView.className === "list hide") {
+    cartView.className = "list show";
+  } else {
+    cartView.className = "list hide";
+  }
+};
 
+let cartArray = [];
+const addToCart = value => {
+  products.map((element, index) => {
+    if (value === element._id) {
+      cartArray.push(element);
+    }
+  });
+  removeProducts("Cart");
+  productList(cartArray, "Cart");
+};
 
-//r2pz
+//remove products
+const removeProducts = list => {
+  let containerNames = document.getElementById("names" + list);
+  let containerButtons = document.getElementById("buttons" + list);
+  containerNames.innerHTML = "";
+  containerButtons.innerHTML = "";
+};
